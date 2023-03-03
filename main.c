9598ecfdecb9b813c0f5e8ca19012d8cfd47675f
Naman Chhabra <72180587+Naman2608@users.noreply.github.com>
2023-03-03 09:31:54 +0000

    merged exps

--- main.c
+++ main.c
@@ -8,38 +8,13 @@
 void add_entry()
 {
 	FILE *fp;
-	// int n = 3;
 	int i = 0;
-	// char *text; // 2D array
-	// text = malloc(n * sizeof(char *)); // Allocating only One block of memory for first line
-
-	// char terminate[] = "exit()\n"; // Termination array for exit diary writing
 	printf("\nStart Writting...");
 	// Just Storing Time and a line break as default for every file ------>
 	char Date_Time[50];
 	sprintf(Date_Time, "\n%s\n@%s\nDear Diary,\n\n", DD_Str, TT_Str);
 	printf("%s\n", Date_Time);
-	// text[0] = malloc(10 * sizeof(char));
-	// text[0] = Date_Time;
-	// text[1] = malloc(2 * sizeof(char));
-	// text[1] = "\n";
-	// --------------------------------------------------------------------<
-	// for (i = 2; i < n; i++)
-	// {
-	// text = malloc(1000 * sizeof(char));
-
-	// 	fgets(text[i], 100, stdin);
-
-	// 	if (strcmp(text[i], terminate) == 0)
-	// 	{
-	// 		break;
-	// 	}
-	// 	if (i >= n - 1)
-	// 	{
-	// 		n++;
-	// 		text = realloc(text, (n + 1) * sizeof(char *));
-	// 	}
-	// }
+  
 
 	printf("Enter a multi line string( press ';' to end input)\n");
 	// scanf("%[^;]s", text);
@@ -47,7 +22,7 @@ void add_entry()
 	char *buf = NULL;
 	getdelim(&buf, &n, '\;', stdin);
 
-	// printf("Input String = %s", text);
+
 	if (buf == NULL)
 	{
 		printf("Error: out of memory ...\n");
@@ -60,11 +35,10 @@ void add_entry()
 		printf("Failed to Open the File, Please check the file name\n");
 		exit(0);
 	}
-	// for (int i = 0; i < n - 1; i++)
-	// {
+
 	fputs(Date_Time, fp);
 	fputs(buf, fp);
-	// }
+
 
 	fclose(fp);
 }
